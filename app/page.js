873b6c0fa26243aa636b228830cfe96088b9b1f4
Zachary Maynard <zachary@example.com>
2026-08-13 21:55:46 +0630

    454

--- app/page.js
+++ app/page.js
@@ -38,7 +38,7 @@ export default function Home() {
 
         <div>
           <Image
-            src="/images/profile.jpg"
+            src="profile.jpg"
             alt="Zachary Maynard - GIS & Web Developer"
             width={400}
             height={400}
